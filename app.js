ca182c9249574078aafc78ca42d0a071b552f9b3
Paulo Roberto da Silva <paulorob_s@hotmail.com>
2016-12-30 19:24:36 -0200

    código refatorado pra es6

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-'use strict'
-
-class Server {
-	constructor() {
-		this.express = require('express');
-		this.cors = require('cors');
-		this.favicon = require('serve-favicon');
-		this.controller = require('./controller.js');
-		this.port = process.env.PORT || 3000;
-		this.app = this.express();
-		this.initRoutes();
-		this.initMiddleware();
-		this.initApp();
-	}
-
-	initRoutes (){
-		this.app.get('/', (req, res)=> {
-		    const resultado = this.controller();
-		    res.render(__dirname+'/public/index', {message: "Férias!!"});
-		});
-	}
-
-	initMiddleware (){
-		this.app.use(this.favicon(__dirname + '/public/favicon.ico'));
-		this.app.use(this.cors());
-		this.app.set('view engine', 'ejs');    	
-	}
-
-	initApp (){
-		this.app.listen(this.port, ()=>{
-		    console.log('Rodando na porta '+this.port);
-		});
-	}
-}
-
+const Server = require('./server.js');
 new Server();
+
--- controller.js
+++ controller.js
@@ -1,25 +1,43 @@
-var moment = require('moment');
-var feriados = require('./feriados.js');
+'use strict';
 
+class Controller{
 
-module.exports= function(){
-    var isFeriado = false;
-    var currentDAy = moment().format('dddd');
-    var currentDate = moment(new Date()).format("DD/MM/YYYY");
-
-    feriados.forEach(function(curr){
-        if (curr == currentDate ){
-            isFeriado  = true;
-            return;
-        }
-    });
-    if(isFeriado){
-        return "NÃO";
+    constructor (){
+        this.moment = require('moment');
+        this.feriados = require('./feriados.js');
+        this.isFeriado = false;
+        this.currentDay = this.moment().format('dddd');
+        this.currentMonth = this.moment().format('M');
+        this.confirmIsFeriado();
     }
-    else if(currentDAy == "Sunday" || currentDAy == "Saturday"){
-        return "NÃO";
+
+
+    confirmIsFeriado (){
+        const currentDate = this.moment(new Date()).format("DD/MM/YYYY");
+        this.feriados.forEach(function(curr){
+            if (curr == currentDate ){
+                this.isFeriado  = true;
+                return;
+            }
+        });
     }
-    else{
+
+    confirmIsFerias (){
+        if(this.currentMonth == 12 || this.currentMonth == 1 || this.currentMonth == 2) return true;
+        return false;
+    }
+
+    confirmIsWeekend (){
+        if(this.currentDay == "Sunday" || this.currentDay == "Saturday") return true;
+        return false;
+    }
+
+    getResponse (){
+        if(this.confirmIsFerias()) return "Férias!!!"
+        if(this.isFeriado || this.confirmIsWeekend()) return "NÃO";
         return "SIM";
-    }
-};
+    };
+}
+
+module.exports = Controller;
+
